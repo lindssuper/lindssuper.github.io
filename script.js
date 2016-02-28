1ac2e05db1f5ff32640701f51c51a39a48b41945
lindssuper <lindsaysuperczynski@Lindsays-MacBook-Pro-2.local>
2016-02-28 14:56:24 -0500

    Add more styling and Javascript to homepage
    Added script.js, floated lists

---
+++ script.js
@@ -0,0 +1,34 @@
+window.onload = function(e){
+
+var kittens = 5;
+function turtleFact() {
+	alert("some turtles can live longer than humans");
+}
+
+// turtleFact();
+function temptGang(gangName){
+alert(gangName + "come out and play")
+}
+
+function addNumbers (num1, num2) {
+	alert(num1+num2)
+}
+
+// addNumbers(10, 5)
+
+function square (number) {
+	return number * number;
+}
+
+function displaySquare (squareNumber) {
+	console.log("your number is " + squareNumber)
+}
+
+// displaySquare(square(3));
+
+// var pageNode = document.getElementsByTagName('body')[0];
+// pageNode.innerHTML += '...just adding this bit at the end of the page.'
+
+var title = document.getElementById('open-title');
+title.innerHTML = "Thanks for coming to class";
+}
