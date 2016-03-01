3858638c1ba8be85ce6cabf2484c66ba4408cc7c
lindssuper <lindsaysuperczynski@Lindsays-MacBook-Pro-2.local>
2016-02-29 23:48:52 -0500

    All the changes
    All the things.

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-window.onload = function(e){
+// window.onload = function(e){
 
-var kittens = 5;
-function turtleFact() {
-	alert("some turtles can live longer than humans");
-}
+// var kittens = 5;
+// function turtleFact() {
+// 	alert("some turtles can live longer than humans");
+// }
 
-// turtleFact();
-function temptGang(gangName){
-alert(gangName + "come out and play")
-}
+// // turtleFact();
+// function temptGang(gangName){
+// alert(gangName + "come out and play")
+// }
 
-function addNumbers (num1, num2) {
-	alert(num1+num2)
-}
+// function addNumbers (num1, num2) {
+// 	alert(num1+num2)
+// }
 
-// addNumbers(10, 5)
+// // addNumbers(10, 5)
 
-function square (number) {
-	return number * number;
-}
+// function square (number) {
+// 	return number * number;
+// }
 
-function displaySquare (squareNumber) {
-	console.log("your number is " + squareNumber)
-}
+// function displaySquare (squareNumber) {
+// 	console.log("your number is " + squareNumber)
+// }
 
-// displaySquare(square(3));
+// // displaySquare(square(3));
 
 // var pageNode = document.getElementsByTagName('body')[0];
 // pageNode.innerHTML += '...just adding this bit at the end of the page.'
